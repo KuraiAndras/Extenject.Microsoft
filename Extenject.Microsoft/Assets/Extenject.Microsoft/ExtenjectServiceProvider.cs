21ea3115183c07d7c7831ba933f04f415565bba3
Kurai András <k.andras9642@gmail.com>
2020-11-25 21:39:45 +0100

    Add microsoft extensions dll

---
+++ Extenject.Microsoft/Assets/Extenject.Microsoft/ExtenjectServiceProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Extenject.Microsoft
+{
+    public sealed class ExtenjectServiceProvider : IServiceProvider
+    {
+        public object GetService(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
