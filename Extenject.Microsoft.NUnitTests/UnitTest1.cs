033c7d82fd99f7f5ff9f6bb237cbd45ed704c442
Kurai András <k.andras9642@gmail.com>
2022-09-10 12:18:20 +0200

    add wrapper projects

---
+++ Extenject.Microsoft.NUnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Extenject.Microsoft.NUnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
