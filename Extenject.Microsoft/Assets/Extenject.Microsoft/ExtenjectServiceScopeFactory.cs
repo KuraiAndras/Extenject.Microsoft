1d6d729b1c6c271735889eae06aefcff68187f6f
Sebastian Fey <sebastian@arfinity.io>
2022-12-08 17:51:22 +0100

    fix(ExtenjectServiceScopeFactory): Differentiate between rebinding for implementation type vs factory for scoped services

--- Extenject.Microsoft/Assets/Extenject.Microsoft/ExtenjectServiceScopeFactory.cs
+++ Extenject.Microsoft/Assets/Extenject.Microsoft/ExtenjectServiceScopeFactory.cs
@@ -21,12 +21,77 @@ namespace Extenject.Microsoft
 
             foreach (var service in _scopedTypes)
             {
-                container.Rebind(service.ServiceType)
-                    .To(service.ImplementationType)
-                    .AsSingle();
+                if (service.ImplementationFactory is not null)
+                {
+                    RebindToImplementationFactory(container, service);
+                }
+                else
+                {
+                    RebindToType(container, service);
+                }
             }
 
             return new ExtenjectServiceScope(new ExtenjectServiceProvider(container));
+        }
+
+        private static DiContainer RebindToImplementationFactory(DiContainer container, ServiceDescriptor service)
+        {
+            switch (service.Lifetime)
+            {
+                case ServiceLifetime.Transient:
+                    container.Rebind(service.ServiceType)
+                        .FromMethodUntyped(ctx => service.ImplementationFactory(ctx.Container.Resolve<IServiceProvider>())).AsTransient();
+                    break;
+                case ServiceLifetime.Scoped:
+                case ServiceLifetime.Singleton:
+                default:
+                    container.Bind(service.ServiceType)
+                        .FromMethodUntyped(ctx => service.ImplementationFactory(ctx.Container.Resolve<IServiceProvider>())).AsSingle();
+                    break;
+            }
+
+            return container;
+        }
+
+        private static DiContainer RebindToType(DiContainer container, ServiceDescriptor service)
+        {
+            switch (service.Lifetime)
+            {
+                case ServiceLifetime.Transient:
+                    container
+                        .Rebind(service.ServiceType)
+                        .To(service.ImplementationType ?? service.ServiceType)
+                        .AsTransient();
+                    break;
+                case ServiceLifetime.Singleton:
+                case ServiceLifetime.Scoped:
+                default:
+                    RebindSingleton(container, service);
+                    break;
+            }
+
+            return container;
+        }
+
+        private static DiContainer RebindSingleton(DiContainer container, ServiceDescriptor service)
+        {
+            if (service.ImplementationInstance == null)
+            {
+                container
+                    .Rebind(service.ServiceType)
+                    .To(service.ImplementationType ?? service.ServiceType)
+                    .AsSingle();
+            }
+            else
+            {
+                //Bind it to the existing instance
+                container
+                    .Rebind(service.ServiceType)
+                    .To(service.ImplementationType ?? service.ServiceType)
+                    .FromInstance(service.ImplementationInstance);
+            }
+
+            return container;
         }
     }
 }
